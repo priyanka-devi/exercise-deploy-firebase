ef664d2a3cf68d8f13867b257f327406a52dcb1d
Priyanka Devi <priyanka.devi@ui.ac.id>
2023-06-25 11:34:14 +0700

    Change url in task js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://exercise-deploy-firebase-83fea.web.app/" // TODO: replace this
 const studentName = "Priyanka Devi" // TODO: replace this
 const studentId = "FE4529989" // TODO: replace this
 
